79bf5a72c737189910e089f2fcd9b78f07933241
Ariane Bonnel <abonnel@z3r2p3.42lyon.fr>
2021-04-02 10:26:20 +0200

    Programme fonctionnel

--- main.c
+++ main.c
@@ -1,9 +1,15 @@
-#include <stdio.h> //printf
 #include <unistd.h> //write, lseek, close, read
 #include <sys/types.h> //lseek, open
 #include <stdlib.h> //malloc
 #include <sys/stat.h> //open
 #include <fcntl.h> //open
+
+
+
+
+#include <stdio.h> //printf A ENLEVEEEEEEER
+
+
 
 /*
  * *buf => dans les prototypes de fonctions ==> série de caractères
@@ -63,28 +69,28 @@
 
 void my_putchar(char c)
 {
-    write(1, &c, 1);
-    //write(1 = sortie standard, &c = adresse où est stockée la variable, 1 = on n'affiche qu'un seul caractère)
+	write(1, &c, 1);
+	//write(1 = sortie standard, &c = adresse où est stockée la variable, 1 = on n'affiche qu'un seul caractère)
 }
 
 // *str = pointeur => commence au 1er caractère d'une string et s'arrête au \0 
 void my_putstr(char*   str)
 {
-    int i;
-    i=0;
-    while(str[i])
-    {
-        my_putchar(str[i]);
-        i++;
-    }
+	int i;
+	i=0;
+	while(str[i])
+	{
+		my_putchar(str[i]);
+		i++;
+	}
 }
 
 int my_strlen(char*    str) {
-    int i;
+	int i;
 
-    for (i = 0; str[i] != '\0'; ++i);
+	for (i = 0; str[i] != '\0'; ++i);
 
-    return (i);
+	return (i);
 }
 
 char	*reverse_name(char *arg)
@@ -122,7 +128,7 @@ int *create_index_table(char *content)
 	int		i = 0;
 	int		y;
 	int 	*indexes;
-	
+
 	while (content[i])	
 	{
 		if (content[i] == '\n')
@@ -157,42 +163,64 @@ int *create_index_table(char *content)
 	return (indexes);
 }
 
-/*
-void line_to_write(int fd_output, int *indexes, int nb_of_chars, char *content)
-{
-	int		i;
-	int		y;
-	int		inf;
-	int		sup;
-	
-	i = 0;
-	y = i + 1;
-	while (indexes[y] != -1)
-	{
-		inf = indexes[i];
-		sup = indexes[y];
-		if (content[inf] < content[sup])
-		{
-			while (indexes[y] == '-2')
-			{
-				y++;
-			}
-			if (indexes[y] == -1)
-			{
-				return (inf);
-			}
-			sup = indexes[y];
-		}
-		else if (content[inf] == content[sup])
-		{
-		
-		}
-		else if (content[inf] > content[sup])
-		{
 
+void write_line(int fd_output, char *content, int word_start)
+{
+	while (content[word_start] != '\n')//on ecrit dans fichier depuis debut du mot jusau'au \n
+	{
+		write(fd_output, &(content[word_start]), 1);
+		word_start++;
+	}
+	write(fd_output, "\n", 1);//on ajoute un \n a la fin pour aller a la ligne avant prochaine ligne
+}
+
+void sort_lines(int fd_output, char *content, int *indexes)
+{
+	int		ascii;
+	int		i;
+	int		word_start;
+
+	ascii = 32; //ascii for space, osef des ascii précédents qui sont des char non imprimables
+	i = 0;//pour plus de clarté, word_start contient index de la premiere lettre du mot//pour plus de clarté, word_start contient index de la premiere lettre du mot
+	while (ascii <= 126) //fait tous les char speciaux et les chiffres
+	{
+		while (indexes[i] != -1)//condition d'arret, quand a checké tous les mots
+		{
+			word_start = indexes[i];//pour plus de clarté, word_start contient index de la premiere lettre du mot
+			if (content[word_start] == ascii)
+			{
+				write_line(fd_output, content, word_start);
+			}
+			i++;
+		}
+		i = 0;
+		ascii++;
+		if (ascii == 65)//on saute les lettres majuscules
+		{
+			ascii = 91;
+		}
+		if (ascii == 97)//on saute les lettres minuscules
+		{
+			ascii = 123;;
 		}
 	}
-}*/
+	ascii = 65;
+	while (ascii < 90) //checker lettres maj ET min 
+	{
+		while (indexes[i] != -1)
+		{
+			word_start = indexes[i];
+			//ascii + 32 car il y a 32 entre la majuscule d'une lettre et sa minuscule sur la table
+			if (content[word_start] == ascii || content[word_start] == ascii + 32)
+			{
+				write_line(fd_output, content, word_start);
+			}
+			i++;
+		}
+		ascii++;
+		i = 0;
+	}
+}
 
 int main(int argc, char **argv) {
 	int		fd_input = 0;
@@ -208,12 +236,12 @@ int main(int argc, char **argv) {
 	if ((fd_input = open(argv[1], O_RDONLY)) == -1)
 		return -1;
 	file_name = reverse_name(argv[1]);
-//	printf("file_name = %s\n", file_name);
+	//	printf("file_name = %s\n", file_name);
 	if ((fd_output = open(file_name, O_WRONLY | O_APPEND | O_CREAT, 0777)) == -1)
 		return -1;
 	free(file_name);
-//lseek renvoi le nombre de char dans le fichier (car on place le curseur de
-//lecture a la fin
+	//lseek renvoi le nombre de char dans le fichier (car on place le curseur de
+	//lecture a la fin
 	nb_of_chars = lseek(fd_input, 0, SEEK_END);
 	//printf("nb_of_chars = %d\n", nb_of_chars);
 	if (nb_of_chars == 0)	
@@ -221,18 +249,22 @@ int main(int argc, char **argv) {
 		my_putstr("Input file is empty");
 		return 0;
 	}
-//creer string qui peux accueillir le contenu du fichier
+	//creer string qui peux accueillir le contenu du fichier
 	content = malloc(sizeof(char) * nb_of_chars);
-//je me remet au debut du fichier
+	//je me remet au debut du fichier
 	lseek(fd_input, 0, SEEK_SET);
-//enregistrement du contenu du fichier dans content
+	//enregistrement du contenu du fichier dans content
 	read(fd_input, content, nb_of_chars);
-//	printf("content = %s\n", content);
+	//	printf("content = %s\n", content);
 	indexes = create_index_table(content);
-	//index = line_to_write(fd_output, indexes, nb_of_chars, content);
+	//la fonction suivante classe selon premier char du mot et l'ecrit dans le fichier
+	sort_lines(fd_output, content, indexes);
 	close(fd_input);
 	close(fd_output);
 	free(content);
-    return 0;
+	free(indexes);
+	return 0;
 }
 
+//attention a bien suppriemer le fichier txt.unetnoc entre deux appels du programme sinon
+//les outputs vont s'ajouter les uns a la suite des autres
